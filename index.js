805e448af424f3900543e9f4bde35db883e94941
cristianocorrea3 <cristiano.moraes4@etec.sp.gov.br>
2023-03-27 22:14:04 -0300

    outro commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express  =require('express');
 
 const app = express();
 
+app.get('/teste', (req, res)=>{
+    res.send('TESTE!!!');
+});
+
 app.listen(3000, ()=>{
     console.log('SERVIDOR HTTP RODANDO EM - http://localhost:3000');
 });
